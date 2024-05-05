be40576ed3d02f94cdf9d35e28422f6424173761
Blake Castleman <blake@blakes-mbp-6.mynetworksettings.com>
2024-05-05 18:18:14 -0400

    moved all tree creation functions into utils

--- hierarchical_progression_algorithm/Joint_Progression_Algorithm.py
+++ hierarchical_progression_algorithm/Joint_Progression_Algorithm.py
@@ -1,8 +1,9 @@
 import math
-import json
 
-from .utils.tree import Static_Tree, Build_Tree_Data
-from .utils.progression_tree_creation import makeStaticTree, makeProblemProgressionTrees
+from .utils.progression_tree_creation import (
+    makeProblemProgressionTrees,
+    makeConceptProgressionTree,
+)
 
 from .mab_agents.ZPDES_Memory_CONCEPT import ZPDES_Memory_CONCEPT
 from .mab_agents.ZPDES_Memory_PROBLEM import ZPDES_Memory_PROBLEM
@@ -11,15 +12,15 @@ from .mab_agents.ZPDES_Memory_PROBLEM import ZPDES_Memory_PROBLEM
 class Joint_Progression_Algorithm(object):
 
     def __init__(
-        self, curSection: str, problemsFilePath: str = "./Instructor Data/problems.json"
+        self, curSection: str, problemsFilePath: str = "./instructor_data/problems.json"
     ):
 
         # Make Concept and Problem Progression Trees
         self.problemsFilePath = problemsFilePath
 
-        self.progression_tree_concept = self.makeConceptProgressionTree(curSection)
+        self.progression_tree_concept = makeConceptProgressionTree(curSection)
         self.progression_tree_problem_dict, self.problemDifficulties = (
-            self.makeProblemProgressionTrees()
+            makeProblemProgressionTrees(self)
         )
 
         # Make Overarching Concept MAB
@@ -140,96 +141,3 @@ class Joint_Progression_Algorithm(object):
         self.current_progression_algorithm_problem.student_answer_update(
             student_answer_correctness
         )
-
-    def makeConceptProgressionTree(self, section):
-        all_concepts = []
-        if section == "ai_intro_and_defs":
-            all_concepts.extend(["ai_definitions", "four_schools_of_thought"])
-
-            tree_structure = {
-                "Root": ["ai_definitions"],
-                "ai_definitions": ["four_schools_of_thought"],
-                "four_schools_of_thought": [],
-            }
-
-        elif section == "turing_test":
-            all_concepts.extend(["turing_test_definition", "turing_test_examples"])
-
-            tree_structure = {
-                "Root": ["turing_test_definition"],
-                "turing_test_definition": ["turing_test_examples"],
-                "turing_test_examples": [],
-            }
-
-        elif section == "applications_of_ai":
-            all_concepts.extend(["applications_of_ai_examples"])
-
-            tree_structure = {
-                "Root": ["applications_of_ai_examples"],
-                "applications_of_ai_examples": [],
-            }
-        elif section == "history_of_ai":
-            all_concepts.extend(
-                [
-                    "gestation_and_early_enthusiasm_era",
-                    "knowledge_based_era",
-                    "ai_becomes_scientific",
-                ]
-            )
-
-            tree_structure = {
-                "Root": ["gestation_and_early_enthusiasm_era"],
-                "gestation_and_early_enthusiasm_era": ["knowledge_based_era"],
-                "knowledge_based_era": ["ai_becomes_scientific"],
-                "ai_becomes_scientific": [],
-            }
-        elif section == "logical_agents":
-            all_concepts.extend(["wumpus_specific", "logic_review"])
-
-            tree_structure = {
-                "Root": ["wumpus_specific", "logic_review"],
-                "logic_review": [],
-                "wumpus_specific": [],
-            }
-        elif section == "rational_agents":
-            all_concepts.extend(["acting_rationally", "peas", "environment_types"])
-
-            tree_structure = {
-                "Root": ["acting_rationally"],
-                "acting_rationally": ["peas", "environment_types"],
-                "peas": [],
-                "environment_types": [],
-            }
-        elif section == "search":
-            all_concepts.extend(
-                [
-                    "search_definition",
-                    "simple_search",
-                    "constraint_search",
-                    "adversarial_search",
-                    "delete_later_sub",
-                ]
-            )
-
-            tree_structure = {
-                "Root": ["search_definition"],
-                "search_definition": [
-                    "simple_search",
-                    "constraint_search",
-                    "adversarial_search",
-                ],
-                "adversarial_search": ["delete_later_sub"],
-                "simple_search": ["delete_later_sub"],
-                "constraint_search": ["delete_later_sub"],
-                "delete_later_sub": [],
-            }
-        elif section == "ml_intro":
-            all_concepts.extend(["ml_basic_concepts"])
-            tree_structure = {"Root": ["ml_basic_concepts"], "ml_basic_concepts": []}
-        elif section == "perceptron":
-            all_concepts.extend(["perceptron_qs"])
-            tree_structure = {"Root": ["perceptron_qs"], "perceptron_qs": []}
-        else:
-            raise Exception("Invalid section")
-
-        return makeStaticTree(all_concepts, tree_structure)
--- hierarchical_progression_algorithm/utils/progression_tree_creation.py
+++ hierarchical_progression_algorithm/utils/progression_tree_creation.py
@@ -30,6 +30,100 @@ def makeStaticTree(all_concepts, tree_structure):
     )
 
 
+def makeConceptProgressionTree(section):
+    all_concepts = []
+    if section == "ai_intro_and_defs":
+        all_concepts.extend(["ai_definitions", "four_schools_of_thought"])
+
+        tree_structure = {
+            "Root": ["ai_definitions"],
+            "ai_definitions": ["four_schools_of_thought"],
+            "four_schools_of_thought": [],
+        }
+
+    elif section == "turing_test":
+        all_concepts.extend(["turing_test_definition", "turing_test_examples"])
+
+        tree_structure = {
+            "Root": ["turing_test_definition"],
+            "turing_test_definition": ["turing_test_examples"],
+            "turing_test_examples": [],
+        }
+
+    elif section == "applications_of_ai":
+        all_concepts.extend(["applications_of_ai_examples"])
+
+        tree_structure = {
+            "Root": ["applications_of_ai_examples"],
+            "applications_of_ai_examples": [],
+        }
+    elif section == "history_of_ai":
+        all_concepts.extend(
+            [
+                "gestation_and_early_enthusiasm_era",
+                "knowledge_based_era",
+                "ai_becomes_scientific",
+            ]
+        )
+
+        tree_structure = {
+            "Root": ["gestation_and_early_enthusiasm_era"],
+            "gestation_and_early_enthusiasm_era": ["knowledge_based_era"],
+            "knowledge_based_era": ["ai_becomes_scientific"],
+            "ai_becomes_scientific": [],
+        }
+    elif section == "logical_agents":
+        all_concepts.extend(["wumpus_specific", "logic_review"])
+
+        tree_structure = {
+            "Root": ["wumpus_specific", "logic_review"],
+            "logic_review": [],
+            "wumpus_specific": [],
+        }
+    elif section == "rational_agents":
+        all_concepts.extend(["acting_rationally", "peas", "environment_types"])
+
+        tree_structure = {
+            "Root": ["acting_rationally"],
+            "acting_rationally": ["peas", "environment_types"],
+            "peas": [],
+            "environment_types": [],
+        }
+    elif section == "search":
+        all_concepts.extend(
+            [
+                "search_definition",
+                "simple_search",
+                "constraint_search",
+                "adversarial_search",
+                "delete_later_sub",
+            ]
+        )
+
+        tree_structure = {
+            "Root": ["search_definition"],
+            "search_definition": [
+                "simple_search",
+                "constraint_search",
+                "adversarial_search",
+            ],
+            "adversarial_search": ["delete_later_sub"],
+            "simple_search": ["delete_later_sub"],
+            "constraint_search": ["delete_later_sub"],
+            "delete_later_sub": [],
+        }
+    elif section == "ml_intro":
+        all_concepts.extend(["ml_basic_concepts"])
+        tree_structure = {"Root": ["ml_basic_concepts"], "ml_basic_concepts": []}
+    elif section == "perceptron":
+        all_concepts.extend(["perceptron_qs"])
+        tree_structure = {"Root": ["perceptron_qs"], "perceptron_qs": []}
+    else:
+        raise Exception("Invalid section")
+
+    return makeStaticTree(all_concepts, tree_structure)
+
+
 def makeProblemProgressionTrees(joint_progression_algorithm):
 
     progression_tree_problem_dict = dict()
